72979498d8e7902d31002cef192da4e77cdc0d33
Shahram Moafipoor <Shahram Moafipoor>
2016-06-24 13:34:01 -0700

    First commit

---
+++ js/script.js
@@ -0,0 +1,322 @@
+   alert("My name is Elham Akbari");
+
+// Step1
+/*
+  var someMonth;
+
+  var sumerMonth;
+
+  currentMonth
+ 
+  function theMonth 		
+
+  myLibraryAFunction 
+
+*/
+
+
+  // STEP 2 
+/*
+
+ 35  //numeric literal
+"Hello All"   //string literal
+True     // Boolean literal
+null      // literal null value
+
+*/
+
+// Step 3
+
+/*
+var sum=3+3;
+var diff= 6-4;
+*/
+
+// step 4
+
+/*
+var FirstName, LastName, Address, City, State, ZipCode, Age, ReferralSource, Contact;
+*/
+
+// step 5
+
+/*
+
+Var firstName;
+firstName=" Elham";
+Var lastName;
+lastName=" Akbari";
+Var address;
+address=" 11410 Village Ridge RD";
+Var city;
+city=" San Diego";
+Var State;
+State=" California";
+Var ZipCode;
+ZipCode=92131;
+Var age;
+age=38;
+Var ReferalSource;
+ReferalSource="Magazin";
+Var contact;
+contact=8586526987;
+
+*/
+
+
+/*
+
+
+Var firstName=" Elham";
+
+Var lastName=" Akbari";
+
+Var address=" 11410 Village Ridge RD";
+
+Var city=" San Diego";
+
+Var State=" California";
+
+Var ZipCode=92131;
+
+Var age=38;
+
+Var ReferalSource="Magazin";
+
+var contact=8586526987;
+
+*/
+
+/*
+Var firstName=" Elham",lastName=" Akbari", address=" 11410 Village Ridge RD", city=" San Diego", State=" California", ZipCode=92131, age=38, ReferalSource="Magazin", contact=8586526987;
+
+*/
+
+// step 6
+
+
+/*
+Var one= 2+'string';
+
+"2string" 
+
+Var two= false = 'string';
+
+"falsestring"
+
+var three= 2+' false'
+"2false"
+*/
+
+// step 7
+
+
+/*
+car = "BMW"; var car; console.log(car);
+
+var make = "BMW";console.log("The car is a " + make + " " + model + ".");var model = 2002;
+
+  */
+
+// step 8
+
+/*
+
+var someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I am not sure about the former."';
+console.log(someString);
+
+*/
+
+// step 9
+/*
+
+var undefined;
+var isNull;
+
+*/
+
+
+
+// step 10
+
+/*
+
+typeof "Prety"    //string
+
+typeof 38       //number
+
+typeof true      //Boolean
+
+typeof { name: 'Eli', age: 38}
+
+typeof myHome   //undefined
+
+*/
+
+// step 11
+
+/*
+  
+alert("Hello "+ " Zak Ruvalcaba"+ ", welcome to the JavaScript class!");
+
+*/
+
+
+// step 12
+
+/*
+ var name;
+ name="Elham Akbari";
+alert("Hello "+ name + ", welcome to the JavaScript class!");
+
+*/
+
+// step 13
+
+/*
+
+ var name;
+ name="Elham Akbari";
+ var course;
+ course="JavaScript";
+ 
+ alert("Hello "+ name + ", welcome to the "+ course +" class!");
+ 
+ */
+
+
+// step 14
+
+/*
+var name;
+ name="Elham Akbari";
+ var course;
+ course="JavaScript";
+ 
+ alert("Hello "+ name + ",\nwelcome to the "+ course +" class!");
+*/
+
+
+// step 15
+
+/*
+ var name = prompt("Please enter your name");
+
+ alert("Hello "+ name );
+ 
+ */
+
+// step 16
+
+/*
+ var name = prompt("Please enter your name");
+ var course= prompt("Hello " + name + ", Which classes are you taking?");
+ alert("Hello "+ name + " Welcome to the "+ course +" class!");
+ 
+ */
+
+// step 17
+
+/*
+
+    var x;
+    var y;
+    x=10;
+    y=20;
+        
+    console.log(x+y);
+   */ 
+    
+
+// step 18
+
+/*
+
+    var x;
+    x=20;
+    x=x+20;
+    console.log(x);
+    
+    */
+
+
+// step 19
+
+/*
+
+    var x;
+    x=20;
+    x=x*5;
+    console.log(x);
+    
+    */
+
+// step 20
+
+/*
+
+    var x=20%3;
+    x=x/1;
+        
+    console.log(x);
+    
+    */
+
+// step 21
+
+/*
+    var x=20;
+    var y=20;
+    var sum= x+y;
+    var result;
+    if( sum==40 && sum >0) {
+    result = true
+    }
+    else{ result = false}
+    console.log(result);
+    
+    */
+
+// step 22
+
+/*
+
+     var x=5;
+     var y=5;
+     var diff = x-y;
+     var result;
+     if(diff==15 || x<0){ result= true}
+     else{ result= false}
+     console.log(result);
+        
+        */
+ // step 23
+  
+/*
+
+   var widget =  new Object();
+   console.log(typeof widget);
+   
+   */
+
+ // Step 24
+
+/*
+
+var widget = new Object();
+console.log(widget instanceof Object);
+
+*/
+
+//step 25
+
+/*
+
+var widget = new Object();
+console.log(widget instanceof Number);
+
+*/
+
+
+
+
